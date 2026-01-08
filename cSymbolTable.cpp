a6b04e7e65dab277ecd3cb9341be2efaed2e3cfe
Aden <aden.ratliff@oit.edu>
2026-01-08 15:05:36 -0800

    Added Find function

--- cSymbolTable.cpp
+++ cSymbolTable.cpp
@@ -37,3 +37,18 @@ void Insert(cSymbol *sym)
     symbolTable_t &currentScope = scopes.back();
     currentScope.insert({sym->GetName(), sym);
 }
+
+cSymbol *Find(string name)
+{
+    for (int i = scopes.size() - 1; i >= 0; i--)
+    {
+        auto it = scopes[i].find(name);
+        if (it != scopes[i].end())
+        {
+            return it->second;
+        }
+    }
+    return nullptr;
+}
+
+
