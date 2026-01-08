80c16e901bfbaf836d2e2d039b2be92205177843
Aden <aden.ratliff@oit.edu>
2026-01-08 15:08:54 -0800

    Added FindLocal function; Added comments

--- cSymbolTable.cpp
+++ cSymbolTable.cpp
@@ -7,17 +7,21 @@
 #include "cSymbolTable.h"
 #include "cSymbol.h"
 
+// Creates new empty symbol table
 cSymbolTable::cSymbolTable()
 {
     scopes.push_back(symbolTable_t{});
 }
 
+// Adds a new scope and returns pointer 
+// to that scope
 symbolTable_t *increaseScope()
 {
     scopes.push_back(symbolTable_t{});
     return &scopes.back();
 }
 
+// Pops the outermost scope.
 symbolTable_t *decreaseScope()
 {
     if(scopes.size() > 1)
@@ -32,12 +36,14 @@ symbolTable_t *decreaseScope()
     return &scopes.back();
 }
 
+// Inserts new symbol into the current scope
 void Insert(cSymbol *sym)
 {
     symbolTable_t &currentScope = scopes.back();
     currentScope.insert({sym->GetName(), sym);
 }
 
+// Finds a value given a name across all scopes.
 cSymbol *Find(string name)
 {
     for (int i = scopes.size() - 1; i >= 0; i--)
@@ -51,4 +57,14 @@ cSymbol *Find(string name)
     return nullptr;
 }
 
+// Finds a value given a name in the current scope only.
+cSymbol *FindLocal(string name)
+{
+    auto it = scopes.back().find(name);
+    if (it != scopes.back().end())
+    {
+        return it->second;
+    }
+    return nullptr;
+}
 
